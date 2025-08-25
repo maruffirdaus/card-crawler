a4486efb625232b96e9eaccd034989288dd1984a
maruffirdaus <maruffirdaus@outlook.com>
2025-08-25 20:07:49 +0700

    Change runApp on main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:card_crawler/card_crawler_app.dart';
-import 'package:card_crawler/gameplay/ui/scenes/level_3_scene.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -10,17 +9,6 @@ void main() {
     DeviceOrientation.landscapeLeft,
     DeviceOrientation.landscapeRight,
   ]).then((_) {
-    runApp(
-      MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          body: Level3Scene(
-            onComplete: () {
-              runApp(const CardCrawlerApp());
-            },
-          ),
-        ),
-      ),
-    );
+    runApp(const CardCrawlerApp());
   });
 }
